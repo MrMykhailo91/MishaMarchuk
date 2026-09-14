b25a3600df1033f6d9f7b26174066e33cc27a623
MishaMarchuk <marchukmisha91@gmail.com>
2026-09-14 19:26:53 +0300

    Delete lesson30 directory

--- lesson30/maximum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const userSumma = prompt("Введіть суму вкладення.", 10000);
-const userTime = prompt("На скільки місяців оформлюється депозит?", 2);
-const INTEREST_RATE = 5 / 100;
-const result = ((userSumma * INTEREST_RATE) / 12) * userTime;
-const resultUserSumma = Math.round(+userSumma + result);
-alert(`Ваш дохід становить ${resultUserSumma}`);
--- lesson30/normal.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-//Задача 1
-const userMoney = prompt('Ввудіть суму грошей, яка є у Вас в гаманці', '');
-const priceCocolate = prompt('Введіть ціну шоколаду', '');
-const numberChocolates = Math.floor(userMoney / priceCocolate);
-const surrender = userMoney % priceCocolate;
-alert(`Ви купили ${numberChocolates} шоколадок і у Вас залишилося ${surrender} грошей`);
-
